bd2dda7056767f8fd9ac2ef3e70c9bfd554ee013
Origonpages24 <web.origon@gmail.com>
2024-05-15 12:59:33 +0530

    first commit

--- gallery.php
+++ gallery.php
@@ -153,16 +153,10 @@
         </div>
     </div>
 
-
-
-
-
-
     <!-- footer -->
    <?php
         include 'footer.php'
    ?>
-
     <!-- scripting -->
     <script src="JS\script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
